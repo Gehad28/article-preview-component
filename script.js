f8cfc5440ad4fff506b52d3c4305a459d90777a8
جهاد أحمد <gehad0ahmed28@gmail.com>
2024-08-27 01:35:35 +0300

    Descktop Design

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ let btn = document.getElementById("btn");
 let popup = document.getElementById("popup");
 
 btn.addEventListener("click", (e) => {
-    if(popup.style.display == "none"){
+    console.log(popup.style.display);
+    if(!popup.style.display || popup.style.display == "none"){
         popup.style.display = "flex";
     }
     else{
