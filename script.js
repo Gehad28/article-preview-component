60047f547286e6fc4fa20ff6e7047aeb31c5c0fd
جهاد أحمد <gehad0ahmed28@gmail.com>
2024-09-02 22:30:52 +0300

    Mobile View

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 let btn = document.getElementById("btn");
+let icon = document.getElementById("svg");
 let popup = document.getElementById("popup");
+let profile = document.getElementById("profile");
 
 btn.addEventListener("click", (e) => {
-    console.log(popup.style.display);
     if(!popup.style.display || popup.style.display == "none"){
         popup.style.display = "flex";
+        btn.classList.add("bg-color");
+        popup.classList.remove("close");
+        popup.classList.add("open");
     }
     else{
-        popup.style.display = "none";
+        btn.classList.remove("bg-color");
+        popup.classList.remove("open");
+        popup.classList.add("close");
+        popup.addEventListener(
+            'animationend',
+            function handler() {
+                popup.style.display = 'none'; 
+                popup.removeEventListener('animationend', handler);
+            }
+        );
     }
 });
